c2cf1dd96ba586828bb4dac1f9aed19670d36c0c
manindra.mallick <manindra.mallick@IRIS-CSG-1331.irissoftware.com>
2018-02-23 10:36:13 +0530

    committed

--- MyJavaBrains/src/test/StringApp.java
+++ MyJavaBrains/src/test/StringApp.java
@@ -1,12 +1,36 @@
 package test;
 
+import java.util.Arrays;
+
 public class StringApp {
 
 	
+	public static int recursiveMethod(char[] charAr, int i, int count) {
+	    if (ifEquals(charAr[i - 1], charAr[i])) {
+	        count = count + recursiveMethod(charAr, ++i, count);
+	    }
+	    return count;
+	}
+
+	public static boolean ifEquals(char a, char b) {
+	    return a == b;
+	}
+	
 	public static void main(String[] args) {
-		   System.out.println("manindra");
+		
+		String s="ABCABDABCEF";
+		char[] str=s.toCharArray();
+		 Arrays.sort(str);
+		 for (int i = 1; i < str.length;) {
+		        int count = recursiveMethod(str, i, 1);
+		        if (count > 1) {
+		            System.out.println("'" + str[i] + "' comes " + count + " times");
+		            i = i + count;
+		        } else
+		            i++;	
+		
 	}
 		
-    
+	}
 	
 }
