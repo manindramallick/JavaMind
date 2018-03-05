e4731b3b8703ae5bfe96e92f972476a844c500b9
manindra.mallick <manindra.mallick@IRIS-CSG-1331.irissoftware.com>
2018-03-05 18:39:19 +0530

    text

--- MyJavaBrains/src/caveprogram10/App.java
+++ MyJavaBrains/src/caveprogram10/App.java
@@ -7,17 +7,21 @@ public class App {
 	
 	public static void main(String[] args) {
 		
-       HashMap<Employee, String> empHashMap = new HashMap<>();
+       HashMap<Employee, Employee> empHashMap = new HashMap<>();
        
        
        Address address = new Address();
        address.setCity("Delhi");
        Employee employee = new Employee(1, "manindra", address);
-       empHashMap.put(employee, "manindra");
+       
+       empHashMap.put(employee, employee);
+      
        System.out.println(empHashMap.get(employee));
+      
        employee.setEmpName("swarnalata");
        address.setCity("Noida");
        System.out.println(empHashMap.get(employee));
+       
        
        
        
